f146d15c3b73b0b30a039ba15ed2b7ddee1492b2
David Romero <davrock12@hotmail.com>
2020-03-13 12:38:53 -0500

    Se agrega lógica para validar

---
+++ PollyPhrase/C#/PollyPhrases/PollyPhrases/MatchWordValidator.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PollyPhrases
+{
+    public class MatchWordValidator
+    {
+        private string normalizeString(string inputString) => inputString.Normalize(NormalizationForm.FormD).Replace("/[\u0300-\u036f] / g", "");
+        private byte[] getASCIIArray(string inputString) => Encoding.ASCII.GetBytes(inputString);
+        ///
+        ///	Parametro 1: * source: Texto en el que se va a buscar
+        ///	Parametro 2: * textToFind: Frase a buscar 
+        ///	Parametro 3: * caseSensitiveEnabled: Por defecto esta deshabilitado. Validar mayusculas y minusculas
+        ///	Parametro 4: * normalizeStringEnabled: Por defecto esta habilitado. Quitar tildes basados en NFD (Forma de Normalización de Composición Canónica.)
+        ///
+        public string[] matchWord(string source, string textToFind, bool caseSensitiveEnabled = false, bool normalizeStringEnabled = true)
+        {
+            string sourceText = caseSensitiveEnabled ? source : source.ToLower();
+            string textToSearch = caseSensitiveEnabled ? textToFind : textToFind.ToLower();
+            sourceText = normalizeStringEnabled ? normalizeString(sourceText) : source;
+            textToSearch = normalizeStringEnabled ? normalizeString(textToSearch) : textToFind;
+            var vectorForSearch = getASCIIArray(sourceText);
+            var vectorToFind = getASCIIArray(textToSearch);
+            var res = new List<string>();
+
+            for (var i = 0; i < vectorForSearch.Length; i++)
+            {
+                if (vectorForSearch[i] == vectorToFind[0])
+                {
+                    var suspectedInitialPoint = i;
+                    bool wasFullMatch = true;
+                    for (var j = 1; j < vectorToFind.Length; j++)
+                    {
+                        i++;
+                        if (vectorToFind[j] != vectorForSearch[i] || i >= vectorForSearch.Length)
+                        {
+                            wasFullMatch = false;
+                            break;
+                        }
+                    }
+                    if (wasFullMatch)
+                        res.Add((suspectedInitialPoint + 1).ToString());
+                    else
+                        i--;
+                }
+
+            }
+            if (res.Count == 0) res.Add("<no matches>");
+            return res.ToArray();
+        }
+    }
+}
--- PollyPhrase/C#/PollyPhrases/PollyPhrases/Program.cs
+++ PollyPhrase/C#/PollyPhrases/PollyPhrases/Program.cs
@@ -6,7 +6,14 @@ namespace PollyPhrases
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            MatchWordValidator matchWord = new MatchWordValidator();
+            Console.WriteLine(String.Join(", ", matchWord.matchWord("Polly put the kettle on, polly put the kettle on, polly put the kettle on we'll all have tea", "Polly")));
+            Console.WriteLine(String.Join(", ", matchWord.matchWord("Polly put the kettle on, polly put the kettle on, polly put the kettle on we'll all have tea", "polly")));
+            Console.WriteLine(String.Join(", ", matchWord.matchWord("Polly put the kettle on, polly put the kettle on, polly put the kettle on we'll all have tea", "ll")));
+            Console.WriteLine(String.Join(", ", matchWord.matchWord("Polly put the kettle on, polly put the kettle on, polly put the kettle on we'll all have tea", "Ll")));
+            Console.WriteLine(String.Join(", ", matchWord.matchWord("Polly put the kettle on, polly put the kettle on, polly put the kettle on we'll all have tea", "X")));
+            Console.WriteLine(String.Join(", ", matchWord.matchWord("Polly put the kettle on, polly put the kettle on, polly put the kettle on we'll all have tea", "Polx")));
         }
     }
 }
